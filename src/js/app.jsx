5b7ce3961adebff0c65e23ddd2de80cc959f8d5e
Kamil <krzeminski.yt.kamil@gmail.com>
2018-09-18 16:56:11 +0200

    pages

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import styles from '../css/style.css'
+import Footer from './footer.jsx';
+
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+} from 'react-router-dom';
+
+import styles from '../css/style.css';
 
 
 class App extends React.Component {
@@ -25,7 +35,6 @@ class Container extends React.Component {
         return (
             <div className="main-container">
                 <Header/>
-                <Body/>
                 <Footer/>
             </div>
         )
@@ -33,6 +42,20 @@ class Container extends React.Component {
 
 }
 
+class Test extends React.Component {
+        constructor (props) {
+            super(props);
+        }
+
+        render() {
+            return (
+                <div>
+                    hi
+                </div>
+            )
+        }
+
+    }
 
 class Header extends React.Component {
     constructor(props) {
@@ -41,21 +64,29 @@ class Header extends React.Component {
 
     render() {
         return (
-            <div className="header">
-                <div className="header-image"><span className="header-image-text">myWebsite</span></div>
-                <div className="header-menu">
-                    <ul className="header-menu-list">
-                        <li className="header-menu-list-elements">projects</li>
-                        <li className="header-menu-list-elements">workshop</li>
-                        <li className="header-menu-list-elements">user.info</li>
-                        <li className="header-menu-list-elements">contact</li>
-                    </ul>
+            <HashRouter>
+                <div className="header">
+                    <div className="header-image"><span className="header-image-text">myWebsite</span></div>
+                    <div className="header-menu">
+                        <ul className="header-menu-list">
+                            <li className="header-menu-list-elements"><Link to="/">projects</Link></li>
+                            <li className="header-menu-list-elements"><Link to="/workshop">workshop</Link></li>
+                            <li className="header-menu-list-elements"><Link to="/user.info">user.info</Link></li>
+                            <li className="header-menu-list-elements"><Link to="/contact">contact</Link></li>
+                        </ul>
+                    </div>
+                    <Route exact path='/' component={Body}/>
+                    <Route path='/workshop' component={Test}/>
+                    <Route path='/user.info' component={UserInfo}/>
+                    <Route path='/contact' component={Contact}/>
                 </div>
-            </div>
+            </HashRouter>
         )
     }
 
 }
+
+
 class Body extends React.Component {
     constructor(props) {
         super(props);
@@ -67,13 +98,50 @@ class Body extends React.Component {
                 <Project image="image1" title="Software House Idle" description="Software House Idle/Clicker is my first adventure in coding with React, done as a final project of Javascript Bootcamp by Coders Lab. You have to earn money and hire better and better employees to make your company grow huge. Can you get to a famous Gaben?" link="https://github.com/laudini/software-house-idle-game"/>
                 <Project image="image2" title="Minesweeper" description="This is one of the project that I have done after finishing bootcamp. Here I practiced my logic thinking when coding the solving system for minesweeping. Feel free to check, test and comment." link="https://laudini.github.io/minesweeper-lite/dist/"/>
                 <Project image="image3" title="Sudoku" description="Another React project thanks to which I practiced Fetch. Getting an incompleted board and checking it , both using fetch." link="https://laudini.github.io/sudoku-lite/dist/"/>
-                <Project image="image4" title="To do List" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, autem blanditiis enim perspiciatis quasi qui saepe
-    sint sunt tempora temporibus, unde, vero! Corporis id labore optio quis reiciendis sint vero" link="https://laudini.github.io/ToDoList/"/>
+                <Project image="image4" title="To do List" description="This toDoList is an effect of our group effort as an optional project related to JavaScript course by Coders Lab , that all of us attended and completed." link="https://laudini.github.io/ToDoList/"/>
             </div>
         )
     }
 
 }
+
+class Contact extends React.Component {
+        constructor (props) {
+            super(props);
+        }
+
+        render() {
+            return (
+            <div className="contact-main">
+                <div id="pointer"/>
+                <div>e-mail</div>
+                <div>********</div>
+                <div>phone</div>
+                <div>********</div>
+                <div>linkedIn</div>
+                <div>********</div>
+                <div>facebook</div>
+                <div>********</div>
+            </div>
+            )
+        }
+
+    }
+
+class UserInfo extends React.Component {
+        constructor (props) {
+            super(props);
+        }
+
+        render() {
+            return (
+            <div className="user-info-main">
+                Hi, I am Kamil!
+            </div>
+            )
+        }
+
+    }
 
 class Project extends React.Component {
         constructor (props) {
@@ -100,18 +168,18 @@ class Project extends React.Component {
 
     }
 
-class Footer extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>FOOTER</div>
-        )
-    }
-
-}
+// class Footer extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//
+//     render() {
+//         return (
+//             <div>FOOTER</div>
+//         )
+//     }
+//
+// }
 
 
 document.addEventListener('DOMContentLoaded', function () {
