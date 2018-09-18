340145c56195e689274ae7b78f61ca782efa8541
Kamil <krzeminski.yt.kamil@gmail.com>
2018-09-18 12:04:36 +0200

    content

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -42,11 +42,11 @@ class Header extends React.Component {
     render() {
         return (
             <div className="header">
-                <div className="header-image"><span className="header-image-text">laudini projects</span></div>
+                <div className="header-image"><span className="header-image-text">myWebsite</span></div>
                 <div className="header-menu">
                     <ul className="header-menu-list">
-                        <li className="header-menu-list-elements">home & projects</li>
-                        <li className="header-menu-list-elements">current plans</li>
+                        <li className="header-menu-list-elements">projects</li>
+                        <li className="header-menu-list-elements">workshop</li>
                         <li className="header-menu-list-elements">user.info</li>
                         <li className="header-menu-list-elements">contact</li>
                     </ul>
@@ -64,14 +64,11 @@ class Body extends React.Component {
     render() {
         return (
             <div className="body">
-                <Project image="image1" title="Software House Idle" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, autem blanditiis enim perspiciatis quasi qui saepe
-    sint sunt tempora temporibus, unde, vero! Corporis id labore optio quis reiciendis sint vero" link="https://laudini.github.com"/>
-                <Project image="image2" title="Minesweeper" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, autem blanditiis enim perspiciatis quasi qui saepe
-    sint sunt tempora temporibus, unde, vero! Corporis id labore optio quis reiciendis sint vero" link="https://laudini.github.com"/>
-                <Project image="image3" title="Sudoku" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, autem blanditiis enim perspiciatis quasi qui saepe
-    sint sunt tempora temporibus, unde, vero! Corporis id labore optio quis reiciendis sint vero" link="https://laudini.github.com"/>
+                <Project image="image1" title="Software House Idle" description="Software House Idle/Clicker is my first adventure in coding with React, done as a final project of Javascript Bootcamp by Coders Lab. You have to earn money and hire better and better employees to make your company grow huge. Can you get to a famous Gaben?" link="https://github.com/laudini/software-house-idle-game"/>
+                <Project image="image2" title="Minesweeper" description="This is one of the project that I have done after finishing bootcamp. Here I practiced my logic thinking when coding the solving system for minesweeping. Feel free to check, test and comment." link="https://laudini.github.io/minesweeper-lite/dist/"/>
+                <Project image="image3" title="Sudoku" description="Another React project thanks to which I practiced Fetch. Getting an incompleted board and checking it , both using fetch." link="https://laudini.github.io/sudoku-lite/dist/"/>
                 <Project image="image4" title="To do List" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, autem blanditiis enim perspiciatis quasi qui saepe
-    sint sunt tempora temporibus, unde, vero! Corporis id labore optio quis reiciendis sint vero" link="https://laudini.github.com"/>
+    sint sunt tempora temporibus, unde, vero! Corporis id labore optio quis reiciendis sint vero" link="https://laudini.github.io/ToDoList/"/>
             </div>
         )
     }
@@ -87,7 +84,7 @@ class Project extends React.Component {
             return (
                 <section className="body-project">
                     <div className="body-project-image">
-                        <div>{this.props.image}</div>
+                        <div className={this.props.image}></div>
                     </div>
                     <div className="body-project-text">
                         <div>{this.props.title}</div>
